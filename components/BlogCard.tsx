24441b8c004a214622329bef462a8869cfe86c8e
Bamschool <87596687+Bamschool@users.noreply.github.com>
2023-02-15 20:10:58 +0900

    just read

--- components/BlogCard.tsx
+++ components/BlogCard.tsx
@@ -9,14 +9,14 @@ interface Props {
 
 const BlogCard: FC<Props> = ({ title, desc, slug }): JSX.Element => {
   return (
-    <Link href={"/blogs/" + slug}>
-      <a className="block">
-        <div className="bg-green-100 p-2 rounded">
-          <h1 className="text-gray-900 text-3xl font-semibold">{title}</h1>
-          <p className="text-gray-500">{desc}</p>
-        </div>
-      </a>
-    </Link>
+    (<Link href={"/blogs/" + slug} className="block">
+
+      <div className="bg-green-100 p-2 rounded">
+        <h1 className="text-gray-900 text-3xl font-semibold">{title}</h1>
+        <p className="text-gray-500">{desc}</p>
+      </div>
+
+    </Link>)
   );
 };
 
